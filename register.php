2ce02162b299579f6e27363994d85fbc95362444
David Stout <davidgstout63@gmail.com>
2020-03-19 21:21:09 -0400

    Sanitized form code

--- register.php
+++ register.php
@@ -1,50 +1,118 @@
-<!DOCTYPE html>
+<?php 
+
+function sanitizeFormPassword($inputText) {
+	$inputText = strip_tags($inputText);
+	return $inputText;
+}
+
+function sanitizeFormUsername($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	return $inputText;
+}
+
+function sanitizeFormString($inputText) {
+	$inputText = strip_tags($inputText);
+	$inputText = str_replace(" ", "", $inputText);
+	$inputText = ucfirst(strtolower($inputText));
+	return $inputText;
+}
+
+if(isset($_POST['loginButton'])) {
+	//Login button was pressed
+	
+}
+
+if(isset($_POST['registerButton'])) {
+	//Register button was pressed
+	$username = sanitizeFormUsername($_POST['username']);
+	
+	$firstName = sanitizeFormString($_POST['firstName']);
+
+	$lastName = sanitizeFormString($_POST['lastName']);
+
+	$email = sanitizeFormString($_POST['email']);
+
+	$email2 = sanitizeFormString($_POST['email2']);
+
+	$password = sanitizeFormPassword($_POST['password']);
+
+	$password2 = sanitizeFormPassword($_POST['password2']);
+
+}
+
+
+?>
+
+
+
 <html>
 <head>
-	<title>Welcome to Music Player</title>
+	<title>Music Player</title>
 </head>
 <body>
+
 	<div id="inputContainer">
 		<form id="loginForm" action="register.php" method="POST">
 			<h2>Login to your account</h2>
-			<label for="loginUsername">Username</label>
-			<input id="loginUsername" type="text" name="loginUsername" placeholder="Username" required></br>
-			<label for="loginPassword">Password</label>
-			<input id="loginPassword" type="password" name="loginPassword" required>
-			<button type="submit" name="loginButton">Login</button>
+			<p>
+				<label for="loginUsername">Username</label>
+				<input id="loginUsername" name="loginUsername" type="text" placeholder="e.g. bartSimpson" required>
+			</p>
+			<p>
+				<label for="loginPassword">Password</label>
+				<input id="loginPassword" name="loginPassword" type="password" placeholder="Your password" required>
+			</p>
+
+			<button type="submit" name="loginButton">LOG IN</button>
+			
 		</form>
+
+
 
 		<form id="registerForm" action="register.php" method="POST">
 			<h2>Create your free account</h2>
-			<label for="username">Username</label>
-			<input id="username" type="text" name="username" placeholder="Username" required></br>
+			<p>
+				<label for="username">Username</label>
+				<input id="username" name="username" type="text" placeholder="User Name" required>
+			</p>
 
-			<label for="firstName">First Name</label>
-			<input id="firstName" type="text" name="firstName" placeholder="First Name" required></br>
+			<p>
+				<label for="firstName">First name</label>
+				<input id="firstName" name="firstName" type="text" placeholder="First Name" required>
+			</p>
 
-			<label for="lastName">Last Name</label>
-			<input id="lastName" type="text" name="lastName" placeholder="Last Name" required></br>
+			<p>
+				<label for="lastName">Last name</label>
+				<input id="lastName" name="lastName" type="text" placeholder="Last Name" required>
+			</p>
 
-			<label for="email">Email</label>
-			<input id="email" type="email" name="email" placeholder="Email" required></br>
+			<p>
+				<label for="email">Email</label>
+				<input id="email" name="email" type="email" placeholder="">
+			</p>
 
-			<label for="email2">Confirm Email</label>
-			<input id="email2" type="email" name="email2" placeholder="Confirm Email" required></br>
+			<p>
+				<label for="email2">Confirm email</label>
+				<input id="email2" name="email2" type="email" placeholder="">
+			</p>
 
+			<p>
+				<label for="password">Password</label>
+				<input id="password" name="password" type="password" placeholder="Your password">
+			</p>
 
-			<label for="password">Password</label>
-			<input id="password" type="password" name="password" required></br>
+			<p>
+				<label for="password2">Confirm password</label>
+				<input id="password2" name="password2" type="password" placeholder="Your password">
+			</p>
 
-			<label for="password2">Confirm Password</label>
-			<input id="password2" type="password" name="password2" required>
-		</br>
-
-
-
-			<button type="submit" name="loginButton">Sign Up</button>
+			<button type="submit" name="registerButton">SIGN UP</button>
+			
 		</form>
 
 
 	</div>
+
 </body>
 </html>
